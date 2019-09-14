81c19e007122cc251689bee88ad7dbb3eaff11cf
maitreya <maitreya.gruen@gmail.com>
2019-09-14 23:12:32 +0200

    First commit

---
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -0,0 +1,68 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.utils.Align;
+
+
+public class MyGdxGame extends ApplicationAdapter {
+
+	Stage stage;
+	Label label;
+
+	float width = 340;
+	
+	@Override
+	public void create () {
+
+
+		Label.LabelStyle style = new Label.LabelStyle();
+
+		FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("Signika-Bold.ttf"));
+		FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+		parameter.size = 76;
+		BitmapFont bitmapFont = generator.generateFont(parameter);
+		bitmapFont.getData().setScale(1 / 2.23f);
+
+		//bitmapFont.getRegion().getTexture().setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+		//bitmapFont.getData().setScale(1 / GameManager.getI().getScreenRatio());
+
+		style.font = bitmapFont;
+		style.font.getData().markupEnabled = true;
+
+		label = new Label("", style);
+		label.setAlignment(Align.center);
+		label.setWrap(true);
+		label.setDebug(true);
+		label.setY(200);
+
+		label.setWidth(width);
+		System.out.println(label.getWidth());
+		label.setText("Automatisch aufgegeben");
+		label.layout();
+		label.setHeight(label.getPrefHeight());
+
+		stage = new Stage();
+		stage.addActor(label);
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+
+		stage.getViewport().apply();
+		stage.draw();
+
+	}
+
+}
